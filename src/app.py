7baa496cef1ff435bfa91124440553f837c73c26
Ika Wu <wunianzu515@gmail.com>
2024-03-26 23:36:41 +0800

    Fix issue

--- src/app.py
+++ src/app.py
@@ -1,11 +1,30 @@
 import json
 from utils import file, reader, opensearch
 import os
-from .helper import convert_apps_to_documents
+from typing import Any
 
 port = int(os.environ.get("OPENSEARCH_PORT"))
 host = os.environ.get("OPENSEARCH_HOST")
 auth = (os.environ.get("OPENSEARCH_USERNAME"), os.environ.get("OPENSEARCH_PASSWORD"))
+
+
+def convert_apps_to_documents(apps: dict[str, Any]) -> list[dict[str, Any]]:
+    documents = []
+
+    for app in apps:
+        documents.append(
+            {
+                "id": int(app["@id"]),
+                "base_vehicle_id": int(app["BaseVehicle"]["@id"]),
+                "qty": int(app["Qty"]),
+                "part_type_id": int(app["PartType"]["@id"]),
+                "mfr_label": app["MfrLabel"],
+                "position_id": int(app["Position"]["@id"]),
+                "part": app["Part"],
+            }
+        )
+
+    return documents
 
 
 def lambda_handler(event, context):
